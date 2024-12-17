44dd9b810368d6ac94c6c9f253ffbc3e2664f210
root <root@ip-172-31-2-198.ap-south-1.compute.internal>
2024-12-17 07:19:08 +0000

    Updated files

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Build the Docker image from the Dockerfile
 docker-compose build
 
-# Check if the image is successfully built
 if [ $? -eq 0 ]; then
   echo "Docker image built successfully!"
 else
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Deploy the application using docker-compose
 docker-compose up -d
 
-# Check if the container is running
 if [ $? -eq 0 ]; then
   echo "Application deployed and running!"
 else
