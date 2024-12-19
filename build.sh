bd43ee495eeb880b64046771a2019f13fbbe3b8c
root <root@ip-172-31-2-198.ap-south-1.compute.internal>
2024-12-19 05:10:29 +0000

    Updated files

--- build.sh
+++ build.sh
@@ -2,9 +2,3 @@
 
 docker-compose build
 
-if [ $? -eq 0 ]; then
-  echo "Docker image built successfully!"
-else
-  echo "Docker image build failed."
-  exit 1
-fi
--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,3 @@
 
 docker-compose up -d
 
-if [ $? -eq 0 ]; then
-  echo "Application deployed and running!"
-else
-  echo "Deployment failed."
-  exit 1
-fi
